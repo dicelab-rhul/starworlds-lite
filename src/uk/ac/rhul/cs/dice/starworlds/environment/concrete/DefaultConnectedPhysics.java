9afc20027d96dc606cf6b31192978399a3725cd6
Ben Wilkins <ZAVC926@live.rhul.ac.uk>
2017-06-21 14:09:21 +0100

    fixed errors after previous change

--- src/uk/ac/rhul/cs/dice/starworlds/environment/concrete/DefaultConnectedPhysics.java
+++ src/uk/ac/rhul/cs/dice/starworlds/environment/concrete/DefaultConnectedPhysics.java
@@ -1,17 +1,10 @@
 package uk.ac.rhul.cs.dice.starworlds.environment.concrete;
 
-import java.util.Set;
-
-import uk.ac.rhul.cs.dice.starworlds.entities.ActiveBody;
-import uk.ac.rhul.cs.dice.starworlds.entities.PassiveBody;
-import uk.ac.rhul.cs.dice.starworlds.entities.agents.AbstractAgent;
 import uk.ac.rhul.cs.dice.starworlds.environment.physics.AbstractConnectedPhysics;
 
 public class DefaultConnectedPhysics extends AbstractConnectedPhysics {
 
-	public DefaultConnectedPhysics(Set<AbstractAgent> agents,
-			Set<ActiveBody> activeBodies, Set<PassiveBody> passiveBodies) {
-		super(agents, activeBodies, passiveBodies);
+	public DefaultConnectedPhysics() {
+		super();
 	}
-
 }
--- src/uk/ac/rhul/cs/dice/starworlds/environment/concrete/DefaultState.java
+++ src/uk/ac/rhul/cs/dice/starworlds/environment/concrete/DefaultState.java
@@ -1,11 +1,17 @@
 package uk.ac.rhul.cs.dice.starworlds.environment.concrete;
 
+import java.util.Set;
+
+import uk.ac.rhul.cs.dice.starworlds.entities.ActiveBody;
+import uk.ac.rhul.cs.dice.starworlds.entities.PassiveBody;
+import uk.ac.rhul.cs.dice.starworlds.entities.agents.AbstractAgent;
 import uk.ac.rhul.cs.dice.starworlds.environment.base.AbstractState;
 import uk.ac.rhul.cs.dice.starworlds.environment.physics.AbstractPhysics;
 
 public class DefaultState extends AbstractState {
 
-	public DefaultState(AbstractPhysics physics) {
-		super(physics);
+	public DefaultState(Set<AbstractAgent> agents,
+			Set<ActiveBody> activeBodies, Set<PassiveBody> passiveBodies) {
+		super(agents, activeBodies, passiveBodies);
 	}
 }
